5a1fededf618746dad4e032d6e96cbab3a2c0584
apple <2313466338@qq.com>
2018-07-03 17:44:05 +0800

    2018-07-03

--- nodejs-rename/app.js
+++ nodejs-rename/app.js
@@ -8,8 +8,8 @@ const aJson = xl.utils.sheet_to_json(worksheet);
 // console.log(workbook.Sheets[workbook.SheetNames[0]]);
 for (let i = 0; i < aJson.length; i++) {
   // console.log(aJson[i])
-  const A = aJson[i].firstname;
-  const B = aJson[i].lastname;
+  const A = './file/' + aJson[i].firstname;
+  const B = './file/' + aJson[i].lastname;
   fs.rename(A, B, err => {
     if (err) throw err;
     console.log('成功了');
