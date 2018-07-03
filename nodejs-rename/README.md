4670cbc58d1665180ddb0b748e3af1b91f43e036
apple <2313466338@qq.com>
2018-07-03 17:39:28 +0800

    2018-07-03

---
+++ nodejs-rename/README.md
@@ -0,0 +1,3 @@
+# 文件重命名，可根据excel文件进行修改
+# npm install xlsx
+# node app.js
---
+++ nodejs-rename/app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const xl = require('xlsx');
+
+const workbook = xl.readFile('./a.xlsx');
+const sheetNames = workbook.SheetNames;
+const worksheet = workbook.Sheets[sheetNames[0]];
+const aJson = xl.utils.sheet_to_json(worksheet);
+// console.log(workbook.Sheets[workbook.SheetNames[0]]);
+for (let i = 0; i < aJson.length; i++) {
+  // console.log(aJson[i])
+  const A = aJson[i].firstname;
+  const B = aJson[i].lastname;
+  fs.rename(A, B, err => {
+    if (err) throw err;
+    console.log('成功了');
+  })
+}
