ced8e1c75be87402bcf46e58ed6ec62e71aa51ae
apple <2313466338@qq.com>
2018-07-16 18:36:29 +0800

    2018-7-13

--- nodejs-request/app.js
+++ nodejs-request/app.js
@@ -1,20 +1,52 @@
 var koa = require('koa');
+var fs = require('fs')
 var request = require('request');
+var path = require('path');
 var app = new koa();
+var views = require('koa-views');
+var router = require('koa-router')();
 
 var data;
-app.use(async (ctx, next) => {
-  await next();
+// app.use(async (ctx, next) => {
+//   await next();
 
-  if (data) {
-    ctx.response.type = 'text/html';
-    ctx.response.body = data;
-  }
-}) 
+//   if (data) {
+//     // ctx.response.type = 'text/html';
+//     // ctx.response.body = data;
+//     ctx.render('index', {
+//       data: data
+//     })
+//   }
+// }) 
+
+app.use(router.get('/index', async (ctx, next) => {
+  fs.readFile('data.json', 'utf8', function (err, data){
+    if(err) {console.log(err)} 
+    let adata = data;
+  })
+  await ctx.render('index', {
+    data: adata
+  })
+  
+}).routes())
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}))
+
 request('https://cnodejs.org/api/v1/user/imizao', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     data = JSON.parse(body).data;
-    // console.log(data);
+    console.log(data);
+    if(data) {
+      fs.writeFile('data.json', JSON.stringify(data), function (err) {
+        if (err) {
+          console.log('写入出现错误');
+        } else{
+          console.log('写入完成！');
+        }
+        
+      })
+    }
   }
 })
 
