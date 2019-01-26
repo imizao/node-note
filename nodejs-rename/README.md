b65124fc1585a070f0f840600cd33a37cdd8711e
imizao <2313466338@qq.com>
2019-01-26 09:31:45 +0800

    优化

--- nodejs-rename/README.md
+++ nodejs-rename/README.md
@@ -1,3 +1,4 @@
 # 文件重命名，可根据excel文件进行修改
 # npm install xlsx
 # node app.js
+# 注意，修改文件时需要找好路径
--- nodejs-rename/app.js
+++ nodejs-rename/app.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const xl = require('xlsx');
+const fileName = 'file'
 
-const workbook = xl.readFile('./a.xlsx');
+const workbook = xl.readFile(`./file/${fileName}.xlsx`);
 const sheetNames = workbook.SheetNames;
 const worksheet = workbook.Sheets[sheetNames[0]];
 const aJson = xl.utils.sheet_to_json(worksheet);
 // console.log(workbook.Sheets[workbook.SheetNames[0]]);
 for (let i = 0; i < aJson.length; i++) {
   // console.log(aJson[i])
-  const A = './file/' + aJson[i].firstname;
-  const B = './file/' + aJson[i].lastname;
-  fs.rename(A, B, err => {
+  const A = `./${fileName}/${aJson[i].firstname}` ;
+  const B = `./${fileName}/${aJson[i].lastname}`;
+  fs.rename(B, A, err => {
     if (err) throw err;
     console.log('成功了');
   })
